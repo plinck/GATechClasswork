c9763eee8ba8997f940e3577cb896c37e08d4856
Paul Linck <paul.linck@gmail.com>
2018-11-22 11:01:44 -0500

    Updated game state

--- 2018-11-20/02-Homework/WordGuess.js
+++ 2018-11-20/02-Homework/WordGuess.js
@@ -1,14 +1,20 @@
 class WordGuess {
-  constructor(word) {
+  constructor(word, guesses) {
+    this.reset(word, guesses);
+  }
+
+  reset(word, guesses) {
+    this.gameInProgress = true;      // true if playing game, false if ended
     this.wins = 0;
     this.wordToGuess = word;
     this.currentGuess = "";
     this.lettersGuessed = "";
-    this.guessesRemaining = 5;
+    this.guessesRemaining = guesses;
     this.lettersCorrectlyGuessed = "";
     this.nbrLettersGuessedCorrect = 0;
     this.lettersIncorrectlyGuessed = "";
     this.nbrLettersGuessedIncorrect = 0;
+    this.guessedCorrectly = false;
 
     this.makeBlankGuess(word);
   }
@@ -39,6 +45,11 @@ class WordGuess {
 
     this.currentGuess = newGuess;
     console.log("newGuess:'" + newGuess + "'");
+
+    if (this.currentGuess == this.wordToGuess.toLowerCase()) {
+      this.guessedCorrectly = true;
+      this.gameInProgress = false;  // end game, win
+    }
   }
 
   makeAGuess(letter) {
@@ -57,14 +68,12 @@ class WordGuess {
     }
 
     // add to letters guessed if they didnt aleady guess it
-    // If they already guessed this letter, give them a pass and another
-    // Try
+    // If they already guessed this letter, give them a pass and another try
     if (this.lettersGuessed.search(lowerLetter) < 0) {
       this.lettersGuessed += lowerLetter;
       this.guessesRemaining -= 1;
 
       if (nbrFound > 0) {
-        console.log(lowerLetter + " is correct");
         // add to letters correctly guessed
         this.lettersCorrectlyGuessed += lowerLetter;
         this.nbrLettersGuessedCorrect += 1;
@@ -72,10 +81,15 @@ class WordGuess {
         this.updateCurrentGuess(indicesFound, lowerLetter);
       } else {
         // add to letters Incorrectly guessed
-        console.log(lowerLetter + " is incorrect");
         this.lettersIncorrectlyGuessed += lowerLetter;
         this.nbrLettersGuessedIncorrect += 1;
       }
+
+      // end the game if no more guesses
+      if (this.guessesRemaining < 1) {
+        this.gameInProgress = false;  // end game, loss
+      }
+
     } else {
       console.log("Already guessed the letter: " + lowerLetter);
     }
@@ -101,23 +115,67 @@ class WordGuess {
     console.log("currentGuess:'" + this.currentGuess + "'");
     console.log("lettersGuessed:'" + this.lettersGuessed + "'");
     console.log("guessesRemaining:" + this.guessesRemaining);
-    console.log(
-      "lettersCorrectlyGuessed:'" + this.lettersCorrectlyGuessed + "'"
-    );
+    console.log("lettersCorrectlyGuessed:'" + this.lettersCorrectlyGuessed + "'");
     console.log("nbrLettersGuessedCorrect:" + this.nbrLettersGuessedCorrect);
-    console.log(
-      "lettersIncorrectlyGuessed:'" + this.lettersIncorrectlyGuessed + "'"
-    );
-    console.log(
-      "nbrLettersGuessedIncorrect:" + this.nbrLettersGuessedIncorrect
-    );
+    console.log("lettersIncorrectlyGuessed:'" + this.lettersIncorrectlyGuessed + "'");
+    console.log("nbrLettersGuessedIncorrect:" + this.nbrLettersGuessedIncorrect);
   }
 }
 
-var wordGuess = new WordGuess("Queen");
+// Word Guess
+var wordGuess = new WordGuess("Queen", 5);
+showState();
+
 console.log("Word to Guess: '" + wordGuess.wordToGuess + "'");
 console.log("Wins: " + wordGuess.wins);
 
+// Get the keyboard input
+document.addEventListener('keyup', function (event) {
+
+  // wait for a spacebar to keep game going if you just finished
+  if (wordGuess.gameInProgress) {
+    // Get the character
+    var charValue = String.fromCharCode(event.keyCode);
+    console.log("You pressed:" + charValue);
+
+    wordGuess.makeAGuess(charValue);
+    showState();
+
+    if ((wordGuess.guessesRemaining < 1) || (wordGuess.guessedCorrectly)) {
+      if (wordGuess.guessedCorrectly) {
+        endGame("You WON! Word is: " + wordGuess.wordToGuess + " - Press Spacebar to restart");
+      } else {
+        endGame("Game Over, word is: " + wordGuess.wordToGuess + " - you lost, Press Spacebar to restart");
+      }
+
+    }
+  } else { // wait for spacebar
+    if (event.keyCode == 32) {
+      wordGuess.reset("Queen", 5);
+      showState();
+    }
+  }
+
+});
+
+function endGame(str) {
+  document.getElementById("gameMessage").innerHTML = str;
+  // Now wait for spacebar
+  continueGame = false;
+}
+
+function showState() {
+  document.getElementById("currentGuess").innerHTML = wordGuess.currentGuess;
+  document.getElementById("guessesRemaining").innerHTML = wordGuess.guessesRemaining;
+  document.getElementById("lettersGuessed").innerHTML = wordGuess.lettersGuessed;
+  document.getElementById("lettersCorrectlyGuessed").innerHTML = wordGuess.lettersCorrectlyGuessed;
+  document.getElementById("lettersIncorrectlyGuessed").innerHTML = wordGuess.lettersIncorrectlyGuessed;
+  if (wordGuess.gameInProgress) {
+    document.getElementById("gameMessage").innerHTML = "playing ...";
+  }
+}
+
+/*
 var guess;
 while (true) {
   guess = prompt("Guess A Letter");
@@ -132,3 +190,4 @@ while (true) {
 
 wordGuess.log();
 wordGuess.print();
+*/
