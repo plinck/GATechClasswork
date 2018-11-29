fba5b68905c8ff71ac16af1ef8fb61a79edce2d6
Paul Linck <paul.linck@gmail.com>
2018-11-29 11:27:55 -0500

    Did the activities for tonight

--- 2018-11-20+27/01-Activities/29-JoanOfArcArrays/Unsolved/joan-of-arc-arrays-unsolved.js
+++ 2018-11-20+27/01-Activities/29-JoanOfArcArrays/Unsolved/joan-of-arc-arrays-unsolved.js
@@ -9,3 +9,15 @@ var joanOfArcInfoValues = [
   "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
   "Stands for French unity and nationalism."
 ];
+
+// Paul Created this sincxe I think that is what we are doing next
+var joanOfArcObject = {
+  "Real Name": "Jehanne la Pucelle.",
+  "Grew Up Where": "Domremy, a village in northeastern France.",
+  "Known For": "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+  "Scars": "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
+  "Symbolism": "Stands for French unity and nationalism."
+};
+
+console.log(joanOfArcObject);
+console.log(joanOfArcObject["Real Name"]);
