57fb79172347b555f69eb62bae089f88f6a444dc
Paul Linck <paul.linck@gmail.com>
2018-12-07 18:40:24 -0500

    Went through activities 1-9 in timers

--- 05-timers 2018-12-8/01-Activities/08-SimpleTimer/Unsolved/simpleTimer-pll.js
+++ 05-timers 2018-12-8/01-Activities/08-SimpleTimer/Unsolved/simpleTimer-pll.js
@@ -10,23 +10,33 @@ var audio = new Audio("raven.mp3");
 
 // ---------------------
 // CODE STEP TWO HERE
+
+setTimeout(fiveSeconds, 5000);
+setTimeout(tenSeconds, 10000);
+setTimeout(timeUp, 15000);
 // ---------------------
 
 // Step 3:
 // Fill in the blanks to these functions.
 function fiveSeconds() {
   // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  $("#time-left").html("<h2>About 10 Seconds Left!");
   // console log 10 seconds left
 }
 
 function tenSeconds() {
   // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  $("#time-left").html("<h2>About 5 Seconds Left!");
+
   // console log 5 seconds left
 }
 
 function timeUp() {
   // in the element with an id of `time-left` add an h2 saying Time's Up!
+  $("#time-left").html("<h2>Times Up!");
+
   // console log done
+  console.log("done");
 
   // The following line will play the audio file above
   audio.play();
