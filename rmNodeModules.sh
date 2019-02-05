fe2ce04757f23789687981662a8de2717f807ec8
Paul Linck <paul.linck@gmail.com>
2019-02-05 08:44:31 -0500

    comments

--- rmNodeModules.sh
+++ rmNodeModules.sh
@@ -1,5 +1,5 @@
-The following command will list out all of the directories find will remove.  Do this from your root development dir.  (like /Projects or /GTATL201811FSF2)
+# The following command will list out all of the directories find will remove.  Do this from your root development dir.  (like /Projects or /GTATL201811FSF2)
 find ./ -name "node_modules" -exec du -s {} \;
 
-The following command will remove the directories.  Same thing as before.  Run it form the room dir that your projects are in.  ****WARNING**** If you run this from the "/" it will find everything on your hard drive named "node_modules" and remove it. Make sure you run the previous command so you know what your are deleting.  No Do Overs.****WARNING****
+# The following command will remove the directories.  Same thing as before.  Run it form the room dir that your projects are in.  ****WARNING**** If you run this from the "/" it will find everything on your hard drive named "node_modules" and remove it. Make sure you run the previous command so you know what your are deleting.  No Do Overs.****WARNING****
 find ./ -name "node_module" -exec rm -rf {} \;
