4804515d740faac1e6208fcba1e0869d0884badb
Paul Linck <paul.linck@gmail.com>
2019-02-19 09:33:34 -0500

    init

---
+++ 14-handlebars/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
@@ -0,0 +1,55 @@
+// Dependencies
+var express = require("express");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+var app = express();
+
+// Routes
+// What routes do you need to have? Which ones are optional?
+// TODO Add your routes here
+app.get("/:operation/:nbr1/:nbr2", function (req, res) {
+  // TODO parse out the variables from the request
+  operation = req.params.operation;
+  // Parameters are received from the URL
+  // TODO make sure they're converted to integers (and not strings)
+  // Parameters are converted to integers
+  nbr1 = parseInt(req.params.nbr1);
+  nbr2 = parseInt(req.params.nbr2);
+
+  // Initialize the result variable to send later
+  var result;
+  // Switch statement chooses operation based on the operation parameter.
+  switch (operation) {
+    // BONUS - How could you use * + etc. inside the app.get()?
+    case "add":
+      result = nbr1 + nbr2;
+      break;
+    case "subtract":
+      result = nbr1 - nbr2;
+      break;
+    case "multiply":
+      result = nbr1 * nbr2;
+      break;
+    case "divide":
+      result = nbr1 / nbr2;
+      break;
+    default:
+      // Handle anything that isn't specified
+      result =
+        "Sorry! The only valid operations are add, subtract, multiply, and divide.";
+  }
+
+  // We return the result back to the user in the form of a string
+  res.send(result.toString());
+
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
