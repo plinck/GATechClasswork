a56caa4f2f9f2e9b54500494b6e50fe5d44b2aca
Paul Linck <paul.linck@gmail.com>
2019-02-19 19:23:43 -0500

    calc

--- 14-handlebars/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
+++ 14-handlebars/01-Activities/01-ExpressCalculator/Unsolved/expressCalculator.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Dependencies
 var express = require("express");
 
@@ -8,20 +10,34 @@ var PORT = process.env.PORT || 8080;
 // Create express app instance.
 var app = express();
 
+app.get("/math/:equation", (req, res) => {
+  let equation = req.params.equation;
+
+  let pattern = /(\d+\s*(\*|\/|\+|\-)\s*)+(\d+\s*)/;
+  pattern = /\d+/g;
+  let NbrArray = equation.match(pattern);
+  console.log(`nbrarray:${NbrArray}`);
+
+  pattern = /(\*|\/|\+|\-)/;
+  let operator = equation.match(pattern);
+
+  console.log(`${NbrArray[0]} ${operator[0]} ${NbrArray[1]} =`);
+});
+
 // Routes
 // What routes do you need to have? Which ones are optional?
 // TODO Add your routes here
-app.get("/:operation/:nbr1/:nbr2", function (req, res) {
+app.get("/:operation/:nbr1/:nbr2",  (req, res) => {
   // TODO parse out the variables from the request
-  operation = req.params.operation;
+  let operation = req.params.operation;
   // Parameters are received from the URL
   // TODO make sure they're converted to integers (and not strings)
   // Parameters are converted to integers
-  nbr1 = parseInt(req.params.nbr1);
-  nbr2 = parseInt(req.params.nbr2);
+  let nbr1 = parseInt(req.params.nbr1);
+  let nbr2 = parseInt(req.params.nbr2);
 
   // Initialize the result variable to send later
-  var result;
+  let result;
   // Switch statement chooses operation based on the operation parameter.
   switch (operation) {
     // BONUS - How could you use * + etc. inside the app.get()?
