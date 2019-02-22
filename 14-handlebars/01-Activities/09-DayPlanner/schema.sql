37630e24eadc7dd744432514108c58191aa62082
Paul Linck <paul.linck@gmail.com>
2019-02-21 21:18:01 -0500

    10--

--- 14-handlebars/01-Activities/09-DayPlanner/schema.sql
+++ 14-handlebars/01-Activities/09-DayPlanner/schema.sql
@@ -1,15 +1,3 @@
-/*
-
-To run this file, we do the following in our Terminal:
-
-1. Go to the directory of this sql file.
-
-2. Get into our mysql console.
-
-3. Run "source schema.sql"
-
-*/
-
 -- Create the database day_planner_db and specified it for use.
 CREATE DATABASE day_planner_db;
 USE day_planner_db;
---
+++ 14-handlebars/01-Activities/10-WatchList/schema.sql
@@ -0,0 +1,14 @@
+-- Create the database day_planner_db and specified it for use.
+CREATE DATABASE moviePlannerDB;
+USE moviePlannerDB;
+
+-- Create the table movies.
+CREATE TABLE movies
+(
+id int NOT NULL AUTO_INCREMENT,
+movie varchar(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+-- Insert a set of records.
+INSERT INTO movies (movie) VALUES ('The 300');
