9b451bbdb9cbd1fabb4215a6ca5f8afa3ac1ba96
Paul Linck <paul.linck@gmail.com>
2019-02-27 09:38:57 -0500

    plural

--- 15-sequelize/01-Activities/04-StarWars/Unsolved/app/model/character.js
+++ 15-sequelize/01-Activities/04-StarWars/Unsolved/app/model/character.js
@@ -6,16 +6,20 @@ var Sequelize = require("sequelize");
 // connection
 var connection = require("../config/connection.js");
 
-// Creates a "Chirp" model that matches up with DB
+// Creates a "Character" model that matches up with DB
 var Character = connection.define("character", {
     routeName: Sequelize.STRING,
     name: Sequelize.STRING,
     role: Sequelize.STRING,
     age: Sequelize.INTEGER,
     forcePoints: Sequelize.INTEGER
-});
+},
+{
+    freezeTableName: true       // Dont pluralize
+}
+);
 
-// Syncs with DB
+// Syncs with D
 Character.sync();
 
 // Makes the Chirp Model available for other files (will also create a table)
