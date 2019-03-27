73548c0629be5626119a2aa87efba56aad81bd66
Paul Linck <paul.linck@gmail.com>
2019-03-27 09:08:21 -0400

    Added activity 19

---
+++ 19-react/01-Activities/07-Stu_TruthyFalsey/Unsolved/index.js
@@ -0,0 +1,18 @@
+// 1. Update this function to use a short circuit evaluation to set a default value for `message` to "Hello World!"
+// Think back to node and how we set a PORT 😉
+
+const logMessage = message => {
+  console.log(message ? message : "Hello World!");
+};
+
+logMessage("bye");
+
+// 2. Update the code below to set `message` using a ternary expression (variable = conditional ? value : otherValue)
+
+const logTired = (beenWorkingAllDay) => {
+  let message = beenWorkingAllDay ? "I'm feeling really tired" : "I'm wide awake!";
+
+  console.log(message);
+};
+
+logTired(true);
